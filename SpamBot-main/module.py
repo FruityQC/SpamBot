b3b4cc11d9fd565263c75d4cdf2859672cc1d875
Young Developper <odjickfelix@gmail.com>
2021-04-10 23:41:30 -0400

    s

--- SpamBot-main/module.py
+++ SpamBot-main/module.py
@@ -2,32 +2,35 @@ import pyautogui
 import time
 import sys
 
-user = input("Choose Spam: "
-          "[1]Bee_Movie "
-          "[2]Baby-Justin_Be-beer ")
+def start():
+    user = input("Choose Spam: "
+            "[1]Bee_Movie "
+            "[2]Baby-Justin_Be-beer ")
 
-if user != '1' and user != '2': 
-    print('[' + user + ']' + " Is an INVALID Input")
-    time.sleep(5)
-    sys.exit()
+    if user != '1' and user != '2': 
+        print('[' + user + ']' + " Is an INVALID Input")
+        time.sleep(5)
+        start()
 
-number = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-for x in number:
-    print(x)
-    time.sleep(1)
+    number = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    for x in number:
+        print(x)
+        time.sleep(1)
 
-print("Spamming...Press CTRL + C in this window to Stop everything")
+    print("Spamming...Press CTRL + C in this window to Stop everything")
 
 
-if user == "1":
-    wrd = open("bees", 'r')
-    for word in wrd:
-        pyautogui.typewrite(word)
-        pyautogui.press("enter")
-else:
-    if user == "2":
-        wrd = open("baby", 'r')
+    if user == "1":
+        wrd = open("bees", 'r')
         for word in wrd:
             pyautogui.typewrite(word)
             pyautogui.press("enter")
-print("Finished")
+    else:
+        if user == "2":
+            wrd = open("baby", 'r')
+            for word in wrd:
+                pyautogui.typewrite(word)
+                pyautogui.press("enter")
+    print("Finished")
+
+start()
